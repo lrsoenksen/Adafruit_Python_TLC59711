93787086818a324122ba616fe2333eef36524903
Steve Pomeroy <steve@staticfree.info>
2018-02-11 01:19:40 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name            = "tlc59711",
+      version         = "1.0.0",
+      description     = "Python library to control Adafruit TLC59711 boards",
+      requires        = ['spidev'],
+      author          = "Tony DiCola",
+      author_email    = "tony@tonydicola.com",
+      maintainer      = "Steve Pomeroy",
+      maintainer_email= "steve@staticfree.info",
+      license         = "MIT",
+      url             = "https://github.com/xxv/Adafruit_Python_TLC59711",
+)
